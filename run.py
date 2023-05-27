78c84d8ae0d72501fac8f3480053b494195f97ac
Joaquim Perez <jivkim@gmail.com>
2023-05-27 14:07:37 +0000

    scarpe, read old data, compare, write new data

--- run.py
+++ run.py
@@ -3,8 +3,32 @@ Description: This file runs the scraper and prints the data to the console
 """
 
 import json
+import sys
 from scraper import scrape
 
-with open("data.json", "w+", encoding="utf-8") as file:
-    data = scrape()
-    json.dump(data, file, indent=4)
+print("Scraping new data")
+new_data = scrape()
+
+if len(new_data) == 0:
+    print("Failed to scrape data, exit")
+    sys.exit()
+
+old_data = []
+with open("data.json", "r", encoding="utf-8") as file:
+    print("Reading old data")
+    try:
+        old_data = json.load(file)
+    except json.decoder.JSONDecodeError as error:
+        print("Failed to read old data from data.json:", error)
+
+if len(old_data) > 0:
+    old_temperature = old_data[0]["temperature"]
+    new_temperature = new_data[0]["temperature"]
+    if old_temperature != new_temperature:
+        print(f"Temperature changed from {old_temperature}° to {new_temperature}°")
+    else:
+        print("Temperature did not change")
+
+with open("data.json", "w", encoding="utf-8") as file:
+    print("Writing new data")
+    json.dump(new_data, file, indent=4)
