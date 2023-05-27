ed58b9d7b981d1b8ea59e8c726c4b09dd6abcc20
Joaquim Perez <jivkim@gmail.com>
2023-05-27 16:35:33 +0200

    check and notifiy also if there is no old data

--- notifier.py
+++ notifier.py
@@ -2,5 +2,11 @@ from config import TRESHOLD
 
 
 def check_and_notify(new_temperature, old_temperature):
-    if new_temperature > old_temperature and new_temperature > TRESHOLD:
+    if old_temperature == new_temperature:
+        print("Temperature did not change")
+        return
+
+    print(f"Temperature changed from {old_temperature}° to {new_temperature}°")
+
+    if new_temperature > old_temperature and new_temperature >= TRESHOLD:
         print("Temperature increased and is above treshold, sending notification")
