380dc5d1a6e43800040c6576f041ec694b0decd9
Joaquim Perez <jivkim@gmail.com>
2023-05-27 13:40:21 +0000

    store data in data.json

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 Description: This file runs the scraper and prints the data to the console
 """
 
+import json
 from scraper import scrape
 
-print(scrape())
+with open("data.json", "w+", encoding="utf-8") as file:
+    data = scrape()
+    json.dump(data, file, indent=4)
