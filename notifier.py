72638761aff96bd2d4f20da2e19613650db4bcf7
Joaquim Perez <jivkim@gmail.com>
2023-05-27 22:39:43 +0200

    move mail subject and body in .env

--- notifier.py
+++ notifier.py
@@ -1,18 +1,18 @@
-from config import TRESHOLD, SMTP_SERVER, SMTP_LOGIN, SMTP_PASSWORD, MAIL_FROM, MAIL_TO
+import config
 from smtplib import SMTP
 from email.message import EmailMessage
 
 def send(new_temperature):
     print("Connect to SMTP server")
-    s = SMTP(SMTP_SERVER)
+    s = SMTP(config.SMTP_SERVER)
     s.starttls()
-    s.login(SMTP_LOGIN, SMTP_PASSWORD)
-    print("Send mail")
+    s.login(config.SMTP_LOGIN, config.SMTP_PASSWORD)
+    print("Send email")
     msg = EmailMessage()
-    msg['Subject'] = "Lake is hot"
-    msg['From'] = MAIL_FROM
-    msg['To'] = MAIL_TO
-    msg.set_content(f"L'eau du lac est à {new_temperature}° aujourd'hui")
+    msg['Subject'] = config.MAIL_SUBJECT
+    msg['From'] = config.MAIL_FROM
+    msg['To'] = config.MAIL_TO
+    msg.set_content(config.MAIL_BODY.format(new_temperature))
     s.send_message(msg)
     print("Email sent")
 
@@ -24,6 +24,6 @@ def check_and_notify(new_temperature, old_temperature):
 
     print(f"Temperature changed from {old_temperature}° to {new_temperature}°")
 
-    if new_temperature > old_temperature and new_temperature >= TRESHOLD:
+    if new_temperature > old_temperature and new_temperature >= config.TRESHOLD:
         print("Temperature increased and is above treshold, sending notification")
         send(new_temperature)
