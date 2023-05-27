f53b4a43b0fb32230b1e2986ca1c8ada8853ea54
Joaquim Perez <jivkim@gmail.com>
2023-05-27 16:44:24 +0200

    fix run.py: what if the file is not found

--- run.py
+++ run.py
@@ -15,16 +15,17 @@ if len(new_data) == 0:
     print("Failed to scrape data, exit")
     sys.exit()
 
-with open(config.FILE, "r", encoding="utf-8") as file:
-    print("Reading old data")
-    try:
+new_temperature = new_data[0]["temperature"]
+
+try:
+    with open(config.FILE, "r+", encoding="utf-8") as file:
+        print("Reading old data")
         old_data = json.load(file)
         old_temperature = old_data[0]["temperature"]
-        new_temperature = new_data[0]["temperature"]
         notifier.check_and_notify(new_temperature, old_temperature)
-    except json.decoder.JSONDecodeError as error:
-        print("Failed to read old data from data.json:", error)
-        notifier.check_and_notify(new_temperature, 0)
+except (FileNotFoundError, json.decoder.JSONDecodeError) as error:
+    print("Failed to read old data from data.json:", error)
+    notifier.check_and_notify(new_temperature, 0)
 
 with open(config.FILE, "w", encoding="utf-8") as file:
     print("Writing new data")
